723ae0c2cce2332e73231f20a862e180ff1d20b9
Michael Krisna Cahyadi <cahyadi.michael.mkc24@gmail.com>
2021-11-26 16:20:58 +0700

    Add Calendar Functionality

--- app/src/main/java/id/ac/umn/promato/CalendarFragment.java
+++ app/src/main/java/id/ac/umn/promato/CalendarFragment.java
@@ -2,11 +2,17 @@ package id.ac.umn.promato;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import java.text.DateFormatSymbols;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -56,9 +62,15 @@ public class CalendarFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        CalendarView calendarView = v.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener((calendarView1, year, month, day) -> {
+            String date = day + " " + new DateFormatSymbols().getMonths()[month] + " " + year + "";
+            Toast.makeText(getActivity(), date, Toast.LENGTH_SHORT).show();
+        });
+
+        return v;
     }
 }
